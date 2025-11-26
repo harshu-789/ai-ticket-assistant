afb9369ddc4bcc3ecc9d23fe56ce20f8de82fa03
Rishesha Harsh <risheshaharsh@gmail.com>
2025-11-26 15:30:39 +0530

    Add ticket routes and Inngest integration for ticket management

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import userRouter from './routes/user.js';
+import ticketRouter from './routes/ticket.js';
+import { serve } from 'inngest/express';
+import {inngest} from './inngest/client.js';
+import { onSignup } from './inngest/functions/on-signup.js';
+import { onTicketCreate } from './inngest/functions/on-ticket-create.js';
+
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,7 +16,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth",userRouter)
-
+app.use("/api/tickets",ticketRouter)
+app.use("/api/inngest", serve({client: inngest, functions: [onSignup, onTicketCreate] }))
 
 mongoose
 .connect(process.env.MONGO_URI )
--- routes/ticket.js
+++ routes/ticket.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { authenticate } from '../middlewares/auth.js';
-import { createTicket, getTicket, getTickets } from '../controllers/ticket.js';
+import { createTicket, getTicket, getTickets,  updateTicket, deleteTicket} from '../controllers/ticket.js';
 
 const router = express.Router();
 
 
-router.get('/tickets', authenticate, getTickets)
-router.post('/create-ticket', authenticate, createTicket)
-router.get('/ticket/:id', authenticate, getTicket)
+router.get('/', authenticate, getTickets)
+router.post('/', authenticate, createTicket)
+router.get('/:id', authenticate, getTicket)
+router.put('/:id', authenticate, updateTicket)
+router.delete('/:id', authenticate, deleteTicket)
 
 export default router;
